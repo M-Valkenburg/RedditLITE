a17ac7311f0e1ee28a42b331f1757858f1c946be
M. Valkenburg <m.valkenburg@live.nl>
2022-06-13 12:34:12 +0200

    Changed colors, added card components for testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
+import Header from './components/Header/Header';
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Card />
+      </div>
     </div>
   );
 }
