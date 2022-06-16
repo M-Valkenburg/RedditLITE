f237825049c31eed0af6d0f5464fd864e284676a
M. Valkenburg <m.valkenburg@live.nl>
2022-06-16 16:21:37 +0200

    Added some props for testing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,23 +2,45 @@ import React from 'react';
 import './Card.css';
 import { FaArrowUp, FaArrowDown, FaComment } from 'react-icons/fa'
 
-export default function Card() {
+export default function Card(props) {
+
     return (
-        <div className="card">
-            <h2>Reddit logo in green</h2>
-            <span className="timestamp"><strong>&#xB7;</strong> u/foobar - 1 hour ago</span>
-            <img src="./images/logo.png" alt="Test"/>
-            <div className="post-info">
-                <div className="votes">
-                    <span><FaArrowUp /></span>
-                    <p>&nbsp;&nbsp;356&nbsp;&nbsp;</p>
-                    <span><FaArrowDown /></span>
-                </div>
-                <div className="comments">
-                    <span><FaComment /></span>
-                    <p>&nbsp;34&nbsp;</p>
+            <div className="card">
+                <h2>{props.title}</h2>
+                <span className="timestamp"><strong>&#xB7;</strong> {props.author_fullname}- {props.approved_at_utc}</span>
+                <img src="./images/logo.png" alt="Test"/>
+                <div className="post-info">
+                    <div className="votes">
+                        <span><FaArrowUp /></span>
+                        <p>&nbsp;&nbsp;{props.ups}&nbsp;&nbsp;</p>
+                        <span><FaArrowDown /></span>
+                    </div>
+                    <div className="comments">
+                        <span><FaComment /></span>
+                        <p>&nbsp;34&nbsp;</p>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 };
+
+// export default function Card() {
+//     return (
+//         <div className="card">
+//             <h2>Reddit logo in green</h2>
+//             <span className="timestamp"><strong>&#xB7;</strong> u/foobar - 1 hour ago</span>
+//             <img src="./images/logo.png" alt="Test"/>
+//             <div className="post-info">
+//                 <div className="votes">
+//                     <span><FaArrowUp /></span>
+//                     <p>&nbsp;&nbsp;356&nbsp;&nbsp;</p>
+//                     <span><FaArrowDown /></span>
+//                 </div>
+//                 <div className="comments">
+//                     <span><FaComment /></span>
+//                     <p>&nbsp;34&nbsp;</p>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// };
