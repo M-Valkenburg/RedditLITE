9fe6be8026038f8e5aef66fb24eec1bb21e68f62
M. Valkenburg <m.valkenburg@live.nl>
2022-06-16 16:17:03 +0200

    Hardcoded subreddit card for testing

---
+++ src/components/SubredditsCard/SubredditsCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './SubredditsCard.css';
+
+export default function Subreddits() {
+    return (
+        <div className="subreddits">
+            <h3>Subreddits</h3>
+            <div className="subs">
+                <p>r/dankmemes</p>
+                <hr />
+                <p>r/memes</p>
+                <hr />
+                <p>r/javascript</p>
+                <hr />
+                <p>r/R6Extraction</p>
+                <hr />
+                <p>r/css</p>
+                <hr />
+                <p>r/ProgrammerHumor</p>
+                <hr />
+                <p>r/news</p>
+                <hr />
+                <p>r/gaming</p>
+                <hr />
+                <p>r/dogecoin</p>
+                <hr />
+            </div>
+        </div>
+    )
+};
