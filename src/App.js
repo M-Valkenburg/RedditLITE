b9f7135c75dfc64ccd58891c4dbc9a22c8189a39
M. Valkenburg <m.valkenburg@live.nl>
2022-06-16 16:21:21 +0200

    changed hardcoded component to reddit component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Card from './components/Card/Card';
-import Subreddits from './components/Subreddits/Subreddits';
+import Reddit from './features/posts/Reddit';
+//import Card from './components/Card/Card';
+import SubredditsCard from './components/SubredditsCard/SubredditsCard';
 
 export default function App() {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <div className="posts">
-          <Card />
-          <Card />
-        </div>
-        <Subreddits />
+        <section className="posts">
+          <Reddit />
+        </section>
+        <SubredditsCard />
       </div>
     </div>
   );
