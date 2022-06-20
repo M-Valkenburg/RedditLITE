58615d14e70023244c9bbf266260a2e06f18e185
M. Valkenburg <m.valkenburg@live.nl>
2022-06-20 12:36:22 +0200

    Added condional for images and video content

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,13 +6,30 @@ import remarkGfm from 'remark-gfm'
 
 export default function Card(props) {
     const { post } = props;
-
-
     let content;
+
     if (post.selftext !== "") {
         content = <div className="content-text"><ReactMarkdown remarkPlugins={[remarkGfm]}>{post.selftext}</ReactMarkdown></div>
-    } else if (post.thumbnail !== "self") {
-        content = <img src={post.thumbnail} alt="" height={post.thumbnail_height} width={post.thumbnail_width}/>
+    }
+   
+    if (post.thumbnail !== "self") {
+        const url = post.url
+        const regex = new RegExp(/\.(jpe?g|png|gif|bmp|webp)$/i);
+
+        if (regex.test(url)) {
+            content = <img className="content-img" src={post.url} alt=""/>
+        } else {
+            content = (            
+                <div className="content-link">
+                    <img src={post.thumbnail} alt=""/>
+                    <a href={post.url} alt={post.title}>{post.url}</a>
+                </div>
+            )
+        }
+    }
+
+    if (post.is_video) {
+        content = <video src={post.media.reddit_video.fallback_url} controls="on"/>
     }
 
     return (
@@ -34,24 +51,3 @@ export default function Card(props) {
             </div>
     )
 };
-
-// export default function Card() {
-//     return (
-//         <div className="card">
-//             <h2>Reddit logo in green</h2>
-//             <span className="timestamp"><strong>&#xB7;</strong> u/foobar - 1 hour ago</span>
-//             <img src="./images/logo.png" alt="Test"/>
-//             <div className="post-info">
-//                 <div className="votes">
-//                     <span><FaArrowUp /></span>
-//                     <p>&nbsp;&nbsp;356&nbsp;&nbsp;</p>
-//                     <span><FaArrowDown /></span>
-//                 </div>
-//                 <div className="comments">
-//                     <span><FaComment /></span>
-//                     <p>&nbsp;34&nbsp;</p>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// };
