b979d80b5398bef6621648232016e91d10a355cc
M. Valkenburg <m.valkenburg@live.nl>
2022-06-22 11:51:26 +0200

    changed conditional for youtube links

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -26,7 +26,7 @@ export default function Card(props) {
             )
     }
 
-    if (post.post_hint === "link" || post.url.includes('/gallery/') || post.url.includes("https://www.youtube.com/")) {
+    if (post.post_hint === "link" || post.url.includes('/gallery/') || post.post_hint === "rich:video") {
         const url = post.url;
         let trimmedLink;
     
