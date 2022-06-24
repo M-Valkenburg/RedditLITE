e3f3ea328cd6e66da31600bfe4663650a06266e4
M. Valkenburg <m.valkenburg@live.nl>
2022-06-24 14:41:16 +0200

    added rendering of subreddits

--- src/components/SubredditsCard/SubredditsCard.js
+++ src/components/SubredditsCard/SubredditsCard.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './SubredditsCard.css';
+import Loader from '..//Loader/Loader';
+import LoadingError from '..//LoadingError/LoadingError';
+import logo from '../../images/logo.png';
+import { setSubreddit } from '../../features/posts/redditSlice';
 
-export default function Subreddits() {
+export default function Subreddits({ subs }) {
+    const dispatch = useDispatch();
+
+    const subreddit = useSelector((state) => state.reddit);
+    const { isLoading, hasError } = subreddit;
+
+    const displayedSubs = subs?.map(sub => {
+        return (
+            <li key={sub.id} onClick={() => dispatch(setSubreddit(sub.display_name))}>
+                <img
+                    className="subreddit-icon"
+                    src={sub.icon_img || logo} 
+                    alt={sub.display_name}
+                />
+                {sub.display_name_prefixed}
+            </li>
+        )
+    });
+
     return (
         <div className="subreddits">
             <h3>Subreddits</h3>
-            <div className="subs">
-                <p>r/dankmemes</p>
-                <hr />
-                <p>r/memes</p>
-                <hr />
-                <p>r/javascript</p>
-                <hr />
-                <p>r/R6Extraction</p>
-                <hr />
-                <p>r/css</p>
-                <hr />
-                <p>r/ProgrammerHumor</p>
-                <hr />
-                <p>r/news</p>
-                <hr />
-                <p>r/gaming</p>
-                <hr />
-                <p>r/dogecoin</p>
-                <hr />
-            </div>
+            <ul className="subs">
+                {isLoading
+                    ? <Loader />
+                    : hasError
+                        ? <LoadingError />
+                        : displayedSubs}
+            </ul>
         </div>
     )
 };
