b363e8f21a2ece03e1826a5fbd3b5e360efd6ae8
M. Valkenburg <m.valkenburg@live.nl>
2022-06-27 11:44:31 +0200

    loader for subreddits

---
+++ src/components/Loader/SubredditsLoader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SubredditsLoader.css';
+
+export default function SubredditsLoader() {
+    return (
+        <div className="subreddits-loader"></div>
+    )
+}
