0e25410fc4f277f77c92c6a39a64342afb2e199a
M. Valkenburg <m.valkenburg@live.nl>
2022-06-27 16:09:27 +0200

    fix to prevent component from reloading

--- src/components/SubredditsCard/SubredditsCard.js
+++ src/components/SubredditsCard/SubredditsCard.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import './SubredditsCard.css';
-import SubredditsLoader from '../Loader/SubredditsLoader';
-import LoadingSubsError from '../LoadingError/LoadingSubsError';
 import logo from '../../images/logo.png';
-import { setSubreddit } from '../../features/posts/redditSlice';
+import { setSearchTerm } from '../../features/posts/redditSlice';
 
 export default function Subreddits({ subs }) {
     const dispatch = useDispatch();
 
-    const subreddit = useSelector((state) => state.reddit);
-    const { isLoading, hasError } = subreddit;
-
     const displayedSubs = subs?.map(sub => {
         return (
-            <li key={sub.id} onClick={() => dispatch(setSubreddit(sub.display_name))}>
+            <li key={sub.id} onClick={() => dispatch(setSearchTerm(sub.display_name))}>
                 <img
                     className="subreddit-icon"
                     src={sub.icon_img || logo} 
@@ -29,12 +24,7 @@ export default function Subreddits({ subs }) {
         <div className="subreddits">
             <h3>Subreddits</h3>
             <ul className="subs">
-                {isLoading
-                    ? <SubredditsLoader />
-                    : hasError
-                        ? <LoadingSubsError />
-                        : displayedSubs
-                }
+                {displayedSubs}
             </ul>
         </div>
     )
