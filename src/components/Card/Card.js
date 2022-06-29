d7cbbcc0fb5e6cee40788304dd94455e6bbecac0
M. Valkenburg <m.valkenburg@live.nl>
2022-06-29 12:12:45 +0200

    working up/downvotes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,84 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './Card.css';
-import { FaArrowUp, FaArrowDown, FaComment } from 'react-icons/fa'
-import { FiExternalLink } from 'react-icons/fi'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import { FaComment } from 'react-icons/fa'
+import { ImArrowUp, ImArrowDown } from 'react-icons/im';
 import moment from 'moment';
+import ContentLoader from './ContentLoader';
 import { setSearchTerm } from '../../features/posts/redditSlice';
 
 export default function Card({ post }) {
     const dispatch = useDispatch();
-    let content;
 
-    const toggleOverlay = () => {
-        const div = document.getElementById(post.id);
-        div.style.display = "none";
-    }
+    const [ vote, setVote ] = useState(0);
+    const [ upvote, setUpvote ] = useState(false);
+    const [ downvote, setDownvote ] = useState(false);
 
-    if (post.selftext !== "") {
-        const expandText = () => {
-            const div = document.getElementById(post.id + 'text');
-            div.style.overflow = "none";
-            div.style.maxHeight = '100%';
+    const handleVote = (value) => {
+        if (value === 1) {
+            setVote(+1);
+            setUpvote(true);
+            setDownvote(false)        
+        } else if (value === -1) {
+            setVote(-1);
+            setDownvote(true);
+            setUpvote(false);
         }
+    } 
 
-        content = (
-            <div className="text-container" id={post.id + 'text'} onClick={expandText}>
-                <div className="content-text"><ReactMarkdown remarkPlugins={[remarkGfm]}>{post.selftext}</ReactMarkdown></div>
-                <div className="text-overlay" id={post.id} onClick={toggleOverlay}></div>
-            </div>
-        )
-    }
- 
-    if (post.post_hint === "image") {
-        content = (
-            <div className="image-container" onClick={toggleOverlay}>
-                <img className="content-img" id="image" src={post.url} alt=""/>
-                {post.over_18 && <div className="nsfw-content" id={post.id}><span>nsfw<br/>click to view</span></div>}
-            </div>
-        )
-    }
-
-    if (post.post_hint === "link" || post.url.includes('/gallery/') || post.post_hint === "rich:video") {
-        const url = post.url;
-        let trimmedLink;
-    
-        if (url.startsWith("https://www.")) {
-            trimmedLink = url.replace("https://www.", "").substring(0, 20) + "...";
-        } else if (url.startsWith("https://www-")) {
-            trimmedLink = url.replace("https://www-", "").substring(0, 20) + "...";
-        } else if (url.startsWith("https://")) {
-            trimmedLink = url.replace("https://", "").substring(0, 20) + "...";
-        }
-
-
-        if (post.thumbnail === "default" || post.thumbnail === "nsfw") {
-            content = <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">{trimmedLink}&nbsp;<FiExternalLink className="link-icon"/></a>
-        } else {
-            content = (            
-                <div className="link-container">
-                    <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">
-                        <div className="thumbnail-container">
-                            <img className="link-image" src={post.thumbnail} alt=""/>
-                            {post.over_18 && <div className="nsfw-content"></div>}
-                        </div>
-                    </a>
-                    <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">{trimmedLink}&nbsp;<FiExternalLink className="link-icon"/></a>
-                </div>
-            )
-        }
-    }
-
-    if (post.post_hint === 'hosted:video') {
-        content = ( 
-            <div className="video-container" onClick={toggleOverlay}>
-                <video className="content-video" src={post.media.reddit_video.fallback_url} controls/>
-                {post.over_18 && <div className="nsfw-content" id={post.id}><span>nsfw<br/>click to view</span></div>}
-            </div>
-        )
-    }
+    useEffect(() => {
+        setVote(vote);
+    }, [vote]);
 
     return (
         <div className="card">
@@ -86,12 +36,12 @@ export default function Card({ post }) {
             <span className="header-info">
                 <strong onClick={() => dispatch(setSearchTerm(post.subreddit))}>{post.subreddit_name_prefixed}</strong> - Posted by {post.author}
             </span>
-            {content}
+            <ContentLoader post={post}/>
             <div className="post-info">
                 <div className="votes">
-                    <span><FaArrowUp /></span>
-                    <p>&nbsp;{post.ups}&nbsp;</p>
-                    <span><FaArrowDown /></span>
+                    <span onClick={() => handleVote(1)} className={upvote && 'upvote-active'}><ImArrowUp/></span>
+                    <p>&nbsp;{post.ups + vote}&nbsp;</p>
+                    <span onClick={() => handleVote(-1)} className={downvote && 'downvote-active'}><ImArrowDown/></span>
                 </div>
                 <span className="time-ago">{moment.unix(post.created_utc).fromNow()}</span>
                 <div className="comments">
