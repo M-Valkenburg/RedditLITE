cedd90102eaba7d2047f7ffd2f023e68367079b5
M. Valkenburg <m.valkenburg@live.nl>
2022-06-29 12:14:22 +0200

    Moved content conditionals to own component

---
+++ src/components/Card/ContentLoader.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './ContentLoader.css';
+import { FiExternalLink } from 'react-icons/fi'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+export default function ContentLoader({ post }) {
+    const toggleOverlay = () => {
+        const div = document.getElementById(post.id);
+        div.style.display = "none";
+    }
+
+    if (post.selftext !== "") {
+        const expandText = () => {
+            const div = document.getElementById(post.id + 'text');
+            div.style.overflow = "none";
+            div.style.maxHeight = '100%';
+        }
+
+        return (
+            <div className="text-container" id={post.id + 'text'} onClick={expandText}>
+                <div className="content-text"><ReactMarkdown remarkPlugins={[remarkGfm]}>{post.selftext}</ReactMarkdown></div>
+                <div className="text-overlay" id={post.id} onClick={toggleOverlay}></div>
+            </div>
+        )
+    }
+ 
+    if (post.post_hint === "image") {
+        return (
+            <div className="image-container" onClick={toggleOverlay}>
+                <img className="content-img" id="image" src={post.url} alt=""/>
+                {post.over_18 && <div className="nsfw-content" id={post.id}><span>nsfw<br/>click to view</span></div>}
+            </div>
+        )
+    }
+
+    if (post.post_hint === 'hosted:video') {
+        return ( 
+            <div className="video-container" onClick={toggleOverlay}>
+                <video className="content-video" src={post.media.reddit_video.fallback_url} controls/>
+                {post.over_18 && <div className="nsfw-content" id={post.id}><span>nsfw<br/>click to view</span></div>}
+            </div>
+        )
+    }
+
+    if (post.post_hint === "link" || post.url.includes('/gallery/') || post.post_hint === "rich:video" || !post.hint) {
+        const url = post.url;
+        let trimmedLink;
+    
+        if (url.startsWith("https://www.")) {
+            trimmedLink = url.replace("https://www.", "").substring(0, 20) + "...";
+        } else if (url.startsWith("https://www-")) {
+            trimmedLink = url.replace("https://www-", "").substring(0, 20) + "...";
+        } else if (url.startsWith("https://")) {
+            trimmedLink = url.replace("https://", "").substring(0, 20) + "...";
+        }
+
+
+        if (post.thumbnail === "default" || post.thumbnail === "nsfw" || post.thumbnail === "") {
+            return (
+                <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">{trimmedLink}&nbsp;<FiExternalLink className="link-icon"/></a>
+            )
+        } else {
+            return (            
+                <div className="link-container">
+                    <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">
+                        <div className="thumbnail-container">
+                            <img className="link-image" src={post.thumbnail} alt=""/>
+                            {post.over_18 && <div className="nsfw-content"></div>}
+                        </div>
+                    </a>
+                    <a target="_blank" href={url} alt={post.title} rel="noreferrer noopener">{trimmedLink}&nbsp;<FiExternalLink className="link-icon"/></a>
+                </div>
+            )
+        }
+    }
+};
