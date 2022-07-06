ab31290b3b232f1835132355f67197b382678aed
M. Valkenburg <m.valkenburg@live.nl>
2022-07-06 11:26:44 +0200

    fetch comments for post

--- src/features/posts/redditSlice.js
+++ src/features/posts/redditSlice.js
@@ -5,18 +5,20 @@ export const fetchPosts = createAsyncThunk(
     async (sub) => {
         const response = await fetch(`https://www.reddit.com/r/${sub}.json`);
         const json = await response.json();
-        return json.data.children.map(post => post.data);
+        return json.data.children.map(post => ({
+            ...post.data,
+            comments: [],
+            commentsLoading: false,
+            commentsError: false,            
+        }))
     }
 );
 
-export const fetchComments = createAsyncThunk(
-    'comments/fetchComments',
-    async (permalink) => {
-        const response = await fetch(`https://www.reddit.com${permalink}.json`);
-        const json = await response.json();
-        return json[1].data.children.map(comment => comment.data);
-    }
-);
+const fetchComments = async (permalink) => {
+    const response = await fetch(`https://www.reddit.com${permalink}.json`);
+    const json = await response.json();
+    return json[1].data.children.map(comment => comment.data)
+};
 
 export const redditSlice = createSlice({
     name: 'reddit',
@@ -26,10 +28,6 @@ export const redditSlice = createSlice({
         hasError: false,
         subreddit: 'popular',
         searchTerm: '',
-        showComments: false,
-        comments: [],
-        commentsLoading: false,
-        commentsError: false,
     },
     reducers: {
         setSubreddit(state, action) {
@@ -40,9 +38,18 @@ export const redditSlice = createSlice({
         setSearchTerm(state, action) {
             state.searchTerm = action.payload;
         },
-        setShowComments(state) {
-            state.showComments = !state.showComments;
-        }
+        commentsPending(state, action) {
+            state.posts[action.payload].commentsloading = true;
+            state.posts[action.payload].commentsError = false;
+        },
+        commentsFulfilled(state, action) {
+            state.posts[action.payload.index].commentsLoading = false;
+            state.posts[action.payload.index].comments = action.payload.comments;
+        },
+        commentsRejected(state, action) {
+            state.posts[action.payload].commentsLoading = false;
+            state.posts[action.payload].commentsError = true;
+        },
     },
     extraReducers: {
         [fetchPosts.pending]: (state) => {
@@ -58,23 +65,27 @@ export const redditSlice = createSlice({
             state.isLoading = false;
             state.hasError = true;
         },
-        [fetchComments.pending]: (state) => {
-            state.commentsLoading = true;
-            state.commentsError = false;
-        },
-        [fetchComments.fulfilled]: (state, action) => {
-            state.comments = action.payload;
-            state.commentsLoading = false;
-            state.commentsError = false;
-        },
-        [fetchComments.rejected]: (state) => {
-            state.commentsLoading = false;
-            state.commentsError = true;
-        }
     }
 });
 
-export const { setSubreddit, setSearchTerm } = redditSlice.actions;
+export const getComments = (index, permalink) => async (dispatch) => {
+    try {
+        dispatch(commentsPending(index));
+        const comments = await fetchComments(permalink);
+        dispatch(commentsFulfilled({ index, comments }));
+    } catch (error) {
+        dispatch(commentsRejected(index));
+    }
+};
+
+export const { 
+    setSubreddit, 
+    setSearchTerm, 
+    setComments, 
+    commentsPending, 
+    commentsFulfilled, 
+    commentsRejected 
+} = redditSlice.actions;
+
 export const selectPosts = (state) => state.reddit.posts;
-export const selectComments = (state) => state.reddit.comments;
 export default redditSlice.reducer;
